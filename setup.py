178ebaacb5ee5cc6afbd46c57c27a1f1414a1e55
dm-wyncode <dm.wyncode@gmail.com>
2016-09-17 19:39:53 +0000

    added text string that parses into html and javascript 	modified:   .gitignore 	modified:   setup.py 	modified:   youtube_editor/time_picker.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 requirements_file = os.path.join(os.curdir, 'requirements.txt')
 
-with open(requirements_file, 'r') as fh: 
+with open(requirements_file, 'r') as fh:
     requirements = [line for line in fh.read().splitlines() if line]
 
 setup(
       name='youtube_editor',
-      version='0.1.4',
+      version='0.1.8',
       install_requires=requirements,
       description='A quick and dirty package for creating YouTube edits in Jupyter notebooks.',
       url='git@github.com:dm-wyncode/pynotebook-youtube-editor.git',
